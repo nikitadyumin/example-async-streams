c75d498364d7e5de64d2547d0cc8e122fb83870a
ndyumin <nikita.dyumin@gmail.com>
2016-03-19 08:11:30 +0500

    add more types

--- examples/index.js
+++ examples/index.js
@@ -3,8 +3,7 @@
  */
 "use strict";
 
-import stream from '../src/index';
-
+import {create, just, fromEvent, fromIterable, fromPromise} from '../src/index';
 const rand = len => () => +(Math.random() * len).toFixed(2);
 const rand10 = rand(10);
 const rand100 = rand(100);
@@ -15,53 +14,54 @@ function* gen(i) {
     }
 }
 
-stream.create(sink => {
+create(sink => {
     setTimeout(()=> sink(1));
     setTimeout(()=> sink(2));
     setTimeout(()=> sink(3));
 }).map(x => x * 3).filter(x => x % 2 !== 0)
     .subscribe(log('1'));
 
-stream.just({a: 123})
-    .flatMap(v => stream.just(v.a))
+just({a: 123})
+    .flatMap(v => just(v.a))
     .subscribe(log('2'));
 
-stream.fromIterable([11, 22, 33])
+fromIterable([11, 22, 33])
     .subscribe(log('3'));
 
-stream.fromPromise(new Promise(res => setTimeout(res, 100, 456)))
+fromPromise(new Promise(res => setTimeout(res, 100, 456)))
     .subscribe(log('4'));
 
-stream.fromEvent(document.querySelector('#b1'), 'click').map(rand10)
+fromEvent(document.querySelector('#b1'), 'click').map(rand10)
     .zip(
-        stream.fromEvent(document.querySelector('#b2'), 'click').map(rand10),
-        stream.fromEvent(document.querySelector('#b3'), 'click').map(rand10))
+        fromEvent(document.querySelector('#b2'), 'click').map(rand10),
+        fromEvent(document.querySelector('#b3'), 'click').map(rand10),
+        (x, y, z) => [x, y, z])
     .subscribe(log('5'));
 
-stream.fromEvent(document.querySelector('#bb1'), 'click').map(rand100)
+fromEvent(document.querySelector('#bb1'), 'click').map(rand100)
     .combine(
-        stream.fromEvent(document.querySelector('#bb2'), 'click').map(rand100),
-        stream.fromEvent(document.querySelector('#bb3'), 'click').map(rand100),
+        fromEvent(document.querySelector('#bb2'), 'click').map(rand100),
+        fromEvent(document.querySelector('#bb3'), 'click').map(rand100),
         (x, y, z) => [x, y, z])
     .subscribe(log('6'));
 
-stream.fromIterable(gen(5))
+fromIterable(gen(5))
     .subscribe(log('7.1'));
 
-stream.fromIterable([11, 22, 33])
+fromIterable([11, 22, 33])
     .subscribe(log('7.2'));
 
-stream.create(sink => {
+create(sink => {
     const i = setInterval(() => sink(rand100()), 1000);
     return () => clearInterval(i);
 }).take(5)
     .subscribe(log('8'));
 
-const it$ = stream.fromIterable([1, 2, 3, 4]);
+const it$ = fromIterable([1, 2, 3, 4]);
 it$.combine(
     it$.scan(0, (x, y) => x + y),
     (x, y)=> [x, y])
     .subscribe(log('9'));
 
-stream.create(sink => sink(1)).startWith(0)
+create(sink => sink(1)).startWith(0)
     .subscribe(log('10'));
--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 /* @flow */
-/**
- * Created by ndyumin on 12.03.2016.
- */
-
 "use strict";
-declare interface Observable<T> {
-subscribe (observer:Observer): Subscription;
-
-map<U> (fn:(x:T) => U): Observable<U>;
-
-filter (predicate: (x: T) => boolean): Observable<T>;
-
-scan<U> (seed:U, fn:(x:U, y:T) => U): Observable<U>;
-
-flatMap<U> (fn:(x:T) => Observable<U>): Observable<U>;
-}
-
-declare interface ObservableModule<T> {
-create (executor:Executor): Observable<T>;
-
-just (value:T): Observable<T>;
-
-fromEvent (el:DomEventEmitter, name:string): Observable;
-
-fromPromise (p:Promise): Observable;
-
-fromIterable (it:Iterable): Observable;
-}
 
 type DomEventEmitter = {
     addEventListener (name:string, listener:Function) : void;
@@ -36,6 +9,30 @@ type DomEventEmitter = {
 type Executor = (observer:Observer) => Subscription;
 type Subscription  = () => void;
 type Observer = (value:any) => void;
+
+declare class Observable<T> {
+    subscribe: (fn:(x:any) => void) => Subscription;
+
+    map: <U>(fn:(x:T) => U) => Observable<U>;
+
+    filter: (predicate:(x:T) => boolean) => Observable<T>;
+
+    scan: <U>(seed:U, fn:(x:U, y:T) => U) => Observable<U>;
+
+    flatMap: <U>(fn:(v:T) => Observable<U>) => Observable<U>;
+
+    startWith: (v:T) => Observable<T>;
+
+    merge: (s:Observable<T>) => Observable<T>;
+
+    take: (n:number) => Observable<T>;
+}
+
+declare function create(executor: Executor):Observable;
+declare function just<T>(v: T):Observable<T>;
+declare function fromEvent<T>(el: DomEventEmitter, name: string):Observable<T>;
+declare function fromPromise<T>(promise: Promise):Observable<T>;
+declare function fromIterable(it: Iterable):Observable;
 
 const runFn = (fn:Function):void => fn();
 
@@ -47,120 +44,118 @@ function first<T> (x:Array<T>):T {
     return x[0];
 }
 
-const stream:ObservableModule = {
-    create: function<T>(executor):Observable<T> {
-        return {
-            subscribe: executor,
+export function create(executor){
+    return {
+        subscribe: executor,
 
-            map: fn => stream.create(sink => executor(v => sink(fn(v)))),
+        map: function (fn) {
+            return create(sink => executor(v => sink(fn(v))))
+        },
 
-            filter: function(predicate: (x: T) => boolean): Observable<T> {
-                return stream.create(sink => executor(v => predicate(v) ? sink(v) : undefined))
-            },
+        filter: (predicate) => create(sink => executor(v => predicate(v) ? sink(v) : undefined)),
 
-            scan: function<U>(seed:U, fn:(x:U, y:T) => U) {
-                return stream.create(sink => executor(y => sink(seed = fn(seed, y))));
-            },
+        scan: function(seed, fn) {
+            return create(sink => executor(y => sink(seed = fn(seed, y))))
+        },
 
-            flatMap: function<U> (fn:(x:T) => Observable<U>):Observable<U> {
-                return stream.create(sink => {
-                    const unsubs = [];
-                    unsubs[0] = executor(x => {
-                        unsubs[1] = fn(x).subscribe(sink);
-                    });
+        flatMap: function (fn) {
+            return create(sink => {
+                const unsubs = [];
+                unsubs[0] = executor(x => {
+                    unsubs[1] = fn(x).subscribe(sink);
+                });
 
-                    return () => unsubs.forEach(runFn);
-                })
-            },
-
-            startWith: v => stream.create(sink => {
-                sink(v);
-                return executor(v => sink(v));
-            }),
-
-            merge: stream2 => stream.create(sink => {
-                const unsubs = [
-                    executor(sink),
-                    stream2.subscribe(sink)
-                ];
                 return () => unsubs.forEach(runFn);
-            }),
+            })
+        },
 
-            take: n => stream.create(sink => {
-                const unsub = executor(v => {
-                    if (--n >= 0) {
-                        sink(v);
-                    } else {
-                        runFn(unsub);
-                    }
-                });
-                return unsub;
-            }),
-
-            combine: (...streams) => {
-                const fn = streams.pop();
-
-                return stream.create(sink => {
-                    const values = [];
-                    const clb = i => val => {
-                        values[i] = val;
-                        if (values.filter(isDefined).length === streams.length + 1) {
-                            sink(fn(...values));
-                        }
-                    };
-                    const unsubs = [executor(clb(0))].concat(streams.map((s, i)=> s.subscribe(clb(i + 1))));
-                    return () => unsubs.forEach(runFn);
-                });
-            },
-
-            zip: (...streams) => {
-                const values = Array.from({length: streams.length + 1}, () => []);
-                return stream.create(sink => {
-                    const clb = i => val => {
-                        values[i].push(val);
-                        if (values.map(first).filter(isDefined).length === streams.length + 1) {
-                            sink(values.map(arr => arr.shift()));
-                        }
-                    };
-                    const unsubs = [executor(clb(0))].concat(streams.map((s, i) => s.subscribe(clb(i + 1))));
-                    return () => unsubs.forEach(runFn);
-                });
-            }
-        };
-    },
-
-    just: function (v) {
-        return stream.create(sink => {
+        startWith: v => create(sink => {
             sink(v);
-            return () => {
-            };
-        });
-    },
+            return executor(v => sink(v));
+        }),
 
-    fromEvent: function (el, event) {
-        return stream.create(sink => {
-            el.addEventListener(event, sink);
-            return () => el.removeEventListener(event, sink);
-        });
-    },
+        merge: stream2 => create(sink => {
+            const unsubs = [
+                executor(sink),
+                stream2.subscribe(sink)
+            ];
+            return () => unsubs.forEach(runFn);
+        }),
 
-    fromPromise: function (promise) {
-        return stream.create(sink => {
-            promise.then(sink);
-            return () => {
-            };
-        })
-    },
+        take: n => create(sink => {
+            const unsub = executor(v => {
+                if (--n >= 0) {
+                    sink(v);
+                } else {
+                    runFn(unsub);
+                }
+            });
+            return unsub;
+        }),
 
-    fromIterable: function (iterable) {
-        return stream.create(sink => {
-            for (let v of iterable) {
-                sink(v);
-            }
-            return () => {
-            };
-        });
-    }
-};
+        combine: (...streams) => {
+            const fn = streams.pop();
 
-export default stream;
+            return create(sink => {
+                const values = [];
+                const clb = i => val => {
+                    values[i] = val;
+                    if (values.filter(isDefined).length === streams.length + 1) {
+                        sink(fn(...values));
+                    }
+                };
+                const unsubs = [executor(clb(0))].concat(streams.map((s, i)=> s.subscribe(clb(i + 1))));
+                return () => unsubs.forEach(runFn);
+            });
+        },
+
+        zip: (...streams) => {
+            const fn = streams.pop();
+
+            const values = Array.from({length: streams.length + 1}, () => []);
+            return create(sink => {
+                const clb = i => val => {
+                    values[i].push(val);
+                    if (values.map(first).filter(isDefined).length === streams.length + 1) {
+                        sink(fn(...values.map(arr => arr.shift())));
+                    }
+                };
+                const unsubs = [executor(clb(0))].concat(streams.map((s, i) => s.subscribe(clb(i + 1))));
+                return () => unsubs.forEach(runFn);
+            });
+        }
+    };
+}
+
+export function just(v) {
+    return create(sink => {
+        sink(v);
+        return () => {
+        };
+    });
+}
+
+export function fromEvent(el, event) {
+    return create(sink => {
+        el.addEventListener(event, sink);
+        return () => el.removeEventListener(event, sink);
+    });
+}
+
+export function fromPromise(promise) {
+    return create(sink => {
+        promise.then(sink);
+        return () => {
+        };
+    })
+}
+
+export function fromIterable(iterable) {
+    return create(sink => {
+        for (let v of iterable) {
+            sink(v);
+        }
+        return () => {
+        };
+    });
+}
