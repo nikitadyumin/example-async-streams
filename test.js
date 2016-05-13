4852304f01fcef8cbaf89758f609938be9c15e75
ndyumin <nikita.dyumin@gmail.com>
2016-05-13 18:11:30 +0500

    add first test

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 import test from 'ava';
+import {just} from './dist/streams';
 
-test(t => {
-    t.deepEqual([1, 2], [1, 2]);
+
+test('just factory', t => {
+    just(1).subscribe(v => t.deepEqual(1, v));
+    just(2).subscribe(v => t.deepEqual(2, v));
+    just('123').subscribe(v => t.deepEqual('123', v));
+    just([1,2,3]).subscribe(v => t.deepEqual([1,2,3], v));
 });
